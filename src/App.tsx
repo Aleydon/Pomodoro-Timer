effe9e7455f7731efdb622f73dce7e7eeb508806
Aleydon <aleydonroberto@gmail.com>
2026-02-06 15:05:56 -0300

    chore(initial commit): initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import { useContext } from 'react';
+import './index.css';
 
-import { Text } from './components/Text';
-import { context } from './context/appProvider';
-
-export default function App() {
-	const { phrase } = useContext(context);
-
+export function App() {
 	return (
-		<div className="flex h-screen flex-col items-center justify-center bg-gradient-to-r from-blue-400 to-purple-300">
-			<h1 className="text-bold text-4xl">Hello World</h1>
-
-			<Text>&darr;</Text>
-
-			<a
-				className="cursor-pointer underline"
-				href="https://github.com/Aleydon"
-				target="_blank"
-				aria-label="github.com/Aleydon"
-				rel="noreferrer"
-			>
-				{phrase}
-			</a>
+		<div>
+			<h2>Hello World</h2>
 		</div>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,27 +2,11 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router';
 
-import App from './App';
-import { NotFound } from './components/NotFound';
-import { AppProvider } from './context/appProvider';
-
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />
-	},
-	{
-		path: '*',
-		element: <NotFound />
-	}
-]);
+import { App } from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<AppProvider>
-			<RouterProvider router={router} />
-		</AppProvider>
+		<App />
 	</React.StrictMode>
 );
