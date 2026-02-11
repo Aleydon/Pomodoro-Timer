206bb78464556d3fd4f331079b4619ed0465944a
Aleydon <aleydonroberto@gmail.com>
2026-02-11 13:26:31 -0300

    refactor(component  refactor): pages and components refatored
    The application's components and pages were separated. Each component and page was refactored.

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,8 @@
 import './styles/global.css';
 import './styles/theme.css';
 
-import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
-
-import { Button } from './components/Button';
-import { Container } from './components/Container';
-import { CountDown } from './components/CountDown';
-import { Cycles } from './components/Cycles';
-import { Footer } from './components/Footer';
-import { Input } from './components/Input';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
+import { Home } from './pages/Home';
 
 export function App() {
-	return (
-		<>
-			<Container>
-				<Logo />
-			</Container>
-			<Container>
-				<Menu />
-			</Container>
-			<Container>
-				<CountDown />
-			</Container>
-
-			<Container>
-				<form className="form" action="">
-					<div className="formRow">
-						<Input
-							label="Task"
-							id="task"
-							type="text"
-							placeholder="Type anything"
-						/>
-					</div>
-					<div className="formRow">
-						<p>Lorem ipsum dolor sit amet.</p>
-					</div>
-					<div className="formRow">
-						<Cycles />
-					</div>
-					<div className="formRow">
-						<Button type="submit" icon={<PlayCircleIcon />} />
-						<Button type="submit" icon={<StopCircleIcon />} color="red" />
-					</div>
-				</form>
-			</Container>
-			<Container>
-				<Footer />
-			</Container>
-		</>
-	);
+	return <Home />;
 }
---
+++ src/components/Form/index.tsx
@@ -0,0 +1,25 @@
+import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
+
+import { Button } from '../Button';
+import { Cycles } from '../Cycles';
+import { Input } from '../Input';
+
+export function Form() {
+	return (
+		<form className="form" action="">
+			<div className="formRow">
+				<Input label="Task" id="task" type="text" placeholder="Type anything" />
+			</div>
+			<div className="formRow">
+				<p>Lorem ipsum dolor sit amet.</p>
+			</div>
+			<div className="formRow">
+				<Cycles />
+			</div>
+			<div className="formRow">
+				<Button type="submit" icon={<PlayCircleIcon />} />
+				<Button type="submit" icon={<StopCircleIcon />} color="red" />
+			</div>
+		</form>
+	);
+}
