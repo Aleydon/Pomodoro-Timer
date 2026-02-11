118b737ce8370765482232426626f3207579ae25
Aleydon <aleydonroberto@gmail.com>
2026-02-11 09:41:35 -0300

    feat(handle theme): add handle theme function and preventDefault her links

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,20 +1,52 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import { useState } from 'react';
 
 import styles from './styles.module.css';
 
+type AvailableThemes = 'light' | 'dark';
+
 export function Menu() {
+	const [theme, setTheme] = useState<AvailableThemes>('dark');
+
+	function handleTheme(e: React.MouseEvent<HTMLAnchorElement>) {
+		e.preventDefault();
+		setTheme(prev => (prev === 'light' ? 'dark' : 'light'));
+	}
+
 	return (
 		<nav className={styles.menu}>
-			<a className={styles.menuLink} href="/">
+			{theme}
+			<a
+				className={styles.menuLink}
+				href="/"
+				title="Go to Home"
+				aria-label="Go to Home"
+			>
 				<HouseIcon size={24} />
 			</a>
-			<a className={styles.menuLink} href="/">
+			<a
+				className={styles.menuLink}
+				href="/"
+				title="History"
+				aria-label="History"
+			>
 				<HistoryIcon size={24} />
 			</a>
-			<a className={styles.menuLink} href="/">
+			<a
+				className={styles.menuLink}
+				href="/"
+				title="Settings"
+				aria-label="Settings"
+			>
 				<SettingsIcon size={24} />
 			</a>
-			<a className={styles.menuLink} href="/">
+			<a
+				className={styles.menuLink}
+				href="/"
+				title="Change Theme"
+				aria-label="Change Theme"
+				onClick={handleTheme}
+			>
 				<SunIcon size={24} />
 			</a>
 		</nav>
