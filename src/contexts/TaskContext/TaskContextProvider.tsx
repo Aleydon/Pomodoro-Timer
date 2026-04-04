239dc0cd5b9a7681cc15cd209df8581729a3c0c8
Aleydon <aleydonroberto@gmail.com>
2026-04-04 20:02:14 -0300

    feat(apply usereducer example): implements useReducer example on contextProvider

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,22 +1,60 @@
-import { useEffect, useState } from 'react';
+import { useReducer, useState } from 'react';
 
 import { initialTaskState } from './initialTaskState';
 import { TaskContext } from './TaskContext';
 
-export function TaskContextProvider({
-	children
-}: {
-	children: React.ReactNode;
-}) {
+export function TaskContextProvider() {
 	const [state, setState] = useState(initialTaskState);
 
-	useEffect(() => {
-		console.warn(state);
-	}, [state]);
+	const [number, dispatch] = useReducer((state, action) => {
+		console.log(state, action);
+
+		switch (action) {
+			case 'INCREMENT':
+				return state + 1;
+			case 'DECREMENT':
+				return state - 1;
+			case 'RESET':
+				return 0;
+			default:
+				break;
+		}
+
+		return state;
+	}, 0);
+
+	// useEffect(() => {
+	// 	console.warn(state);
+	// }, [state]);
 
 	return (
 		<TaskContext.Provider value={{ state, setState }}>
-			{children}
+			{/* {children} */}
+			<h1>The Reducer number state is {number}</h1>
+			<button
+				type="button"
+				onClick={() => {
+					dispatch('INCREMENT');
+				}}
+			>
+				Increment
+			</button>
+			<button
+				type="button"
+				onClick={() => {
+					dispatch('DECREMENT');
+				}}
+			>
+				Decrement
+			</button>
+			<button
+				type="button"
+				onClick={() => {
+					dispatch('RESET');
+				}}
+			>
+				Reset
+			</button>
 		</TaskContext.Provider>
 	);
 }
